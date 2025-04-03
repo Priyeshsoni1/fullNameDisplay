caeb5c22b55e1889f170fcdc6e37b3b7c47612d3
Priyesh Soni <priyesh9792@gmail.com>
2025-04-04 04:49:56 +0530

    Display full name

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import "./App.css";
+import { FullNameDisplay } from "./features/nameDisplay/pages/FullNameDisplay";
+
+export const App = () => {
+  return (
+    <div>
+      <FullNameDisplay />
+    </div>
+  );
+};
---
+++ src/features/nameDisplay/pages/FullNameDisplay.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+
+export const FullNameDisplay = () => {
+  const [formData, setFormData] = useState({});
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const data = Object.fromEntries(formData.entries());
+    setFormData(data);
+
+    console.log("Form Data:", data);
+    e.target.reset(); // Reset form fields after submission
+  };
+
+  return (
+    <div>
+      <h1>Full Name Display</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="firstName">First Name :</label>
+          <input name="firstName" required />
+        </div>
+        <div>
+          <label htmlFor="lastName">Last Name :</label>
+          <input name="lastName" required />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+      <div style={{ margin: 10 }}>
+        {formData.firstName &&
+          formData.lastName &&
+          `Full Name: ${formData.firstName} ${formData.lastName}`}
+      </div>
+    </div>
+  );
+};
