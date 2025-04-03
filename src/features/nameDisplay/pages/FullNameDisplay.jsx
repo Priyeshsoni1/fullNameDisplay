f892b6965d00f662850aaa0bed4eedcfe6917f27
Priyesh Soni <priyesh9792@gmail.com>
2025-04-04 05:29:10 +0530

    added pattern

--- src/features/nameDisplay/pages/FullNameDisplay.jsx
+++ src/features/nameDisplay/pages/FullNameDisplay.jsx
@@ -19,11 +19,16 @@ export const FullNameDisplay = () => {
       <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="firstName">First Name :</label>
-          <input name="firstName" required type="text" />
+          <input
+            name="firstName"
+            required
+            type="text"
+            pattern="^[A-Za-z\s]+$"
+          />
         </div>
         <div>
           <label htmlFor="lastName">Last Name :</label>
-          <input name="lastName" type="text" required />
+          <input name="lastName" type="text" required pattern="^[A-Za-z\s]+$" />
         </div>
         <button type="submit">Submit</button>
       </form>
