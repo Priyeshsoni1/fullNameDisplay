e96d3b139ccafab0b936b025f4867f6beef8c66a
Priyesh Soni <priyesh9792@gmail.com>
2025-04-04 07:02:36 +0530

    Fix the Name

--- src/features/nameDisplay/pages/FullNameDisplay.jsx
+++ src/features/nameDisplay/pages/FullNameDisplay.jsx
@@ -1,40 +1,44 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
-export const FullNameDisplay = () => {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [fullName, setFullName] = useState("");
-  function handleSubmit(e) {
+function App() {
+  const [fname, setFname] = useState("");
+  const [lname, setLname] = useState("");
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const handleSubmit = (e) => {
     e.preventDefault();
-    if (firstName && lastName) setFullName(`${firstName} ${lastName}`);
-  }
+    setIsSubmitted(true);
+  };
+
   return (
-    <div>
+    <div className="App">
       <h1>Full Name Display</h1>
       <form onSubmit={handleSubmit}>
-        <label htmlFor="firstName">First Name: </label>
+        <label>First Name:</label>
         <input
-          value={firstName}
           type="text"
-          id="firstName"
-          name="firstName"
+          value={fname}
+          onChange={(e) => setFname(e.target.value)}
           required
-          onChange={(e) => setFirstName(e.target.value)}
         />
         <br />
-        <label htmlFor="lastName">Last Name: </label>
+        <label>Last Name:</label>
         <input
-          value={lastName}
           type="text"
-          id="lastName"
-          name="lastName"
+          value={lname}
+          onChange={(e) => setLname(e.target.value)}
           required
-          onChange={(e) => setLastName(e.target.value)}
         />
         <br />
         <button type="submit">Submit</button>
       </form>
-      {fullName && <div>Full Name: {fullName}</div>}
+      {isSubmitted && fname && lname ? (
+        <div>
+          Full Name: {fname} {lname}
+        </div>
+      ) : null}
     </div>
   );
-};
+}
+
+export default App;
