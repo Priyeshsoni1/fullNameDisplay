856b1eb621b9a421ff03bc0ef3f9c011920a350c
Priyesh Soni <priyesh9792@gmail.com>
2025-04-04 05:50:03 +0530

    added on onchange

--- src/features/nameDisplay/pages/FullNameDisplay.jsx
+++ src/features/nameDisplay/pages/FullNameDisplay.jsx
@@ -1,31 +1,47 @@
 import React, { useState } from "react";
 
 export const FullNameDisplay = () => {
-  const [formData, setFormData] = useState({});
-
-  const handleSubmit = (e) => {
+  // const [formData, setFormData] = useState({});
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [fullName, setFullName] = useState("");
+  function handleSubmit(e) {
     e.preventDefault();
-    const formData = new FormData(e.target);
-    const data = Object.fromEntries(formData.entries());
-    setFormData(data);
-  };
+    if (firstName && lastName) setFullName(`${firstName} ${lastName}`);
+  }
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   const formData = new FormData(e.target);
+  //   const data = Object.fromEntries(formData.entries());
+  //   setFormData(data);
+  // };
 
   return (
     <div>
       <h1>Full Name Display</h1>
       <form onSubmit={handleSubmit}>
         <label htmlFor="firstName">First Name :</label>
-        <input name="firstName" required type="text" />
+        <input
+          name="firstName"
+          required
+          type="text"
+          onChange={(e) => setFirstName(e.target.value)}
+        />
         <br />
         <label htmlFor="lastName">Last Name :</label>
-        <input name="lastName" type="text" required />
+        <input
+          name="lastName"
+          type="text"
+          required
+          onChange={(e) => setLastName(e.target.value)}
+        />
         <br />
         <button type="submit">Submit</button>
       </form>
-
-      {formData.firstName &&
+      {fullName && <div>Full Name: {fullName}</div>}
+      {/* {formData.firstName &&
         formData.lastName &&
-        `Full Name: ${formData.firstName} ${formData.lastName}`}
+        `Full Name: ${formData.firstName} ${formData.lastName}`} */}
     </div>
   );
 };
